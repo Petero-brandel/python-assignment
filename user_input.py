acb72d807e84590ff4701fa9d0d228f0252a0de8
peterobumneme <peterobumneme334@gmail.com>
2024-08-20 23:41:32 +0100

    my first python assignment commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('what is your name?: ')
+age = int(input('How old are you?: '))
+Location = input('where do you live: ')
+
+print(f'Hello {name} you are {age} years old and you live in {Location}')
